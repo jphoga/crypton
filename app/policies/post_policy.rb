14ab92beb736d3316f1516cff7379bf4217d9cd4
Hiroki Furuichi <aivvinna@gmail.com>
2018-08-21 17:06:30 +0900

    can add comments to posts now

--- app/policies/post_policy.rb
+++ app/policies/post_policy.rb
@@ -5,6 +5,10 @@ class PostPolicy < ApplicationPolicy
     end
   end
 
+  def show?
+    true
+  end
+
   def create?
      true
   end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :posts do
-    resources :comments, only: [:index, :create]
+    resources :comments
   end
   resources :comments, only: [:destroy]
 end
