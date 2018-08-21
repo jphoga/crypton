32f65cf7315527dad7226ee7f59230fa53bd44f2
Jan Hoga <janpaulhoga@gmail.com>
2018-08-21 16:51:37 +0900

    debugged seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,27 +16,27 @@ Portfolio.destroy_all if Rails.env.development?
 
 Demi = User.create (
   {email: "demi.lovato@gmail.com",
-  password: "123456"}
+   password: "123456"}
 )
 
 Jan = User.create (
   {email: "jan@gmail.com",
-  password: "123456"}
+   password: "123456"}
 )
 
 Hiroki = User.create (
   {email: "hiroki@gmail.com",
-  password: "123456"}
+   password: "123456"}
 )
 
 David = User.create (
   {email: "david@gmail.com",
-  password: "123456"}
+   password: "123456"}
 )
 
 Tania = User.create (
   {email: "tania@gmail.com",
-  password: "123456"}
+   password: "123456"}
 )
 
 p "created 5 users"
@@ -47,7 +47,7 @@ Post.create (
    who invest more and more and more.",
    user: User.all.sample,
    likes: [*(1..5)].sample
- }
+   }
 )
 
 Post.create (
@@ -62,9 +62,9 @@ Post.create (
    A project is being advanced with circumspection while solidifying
     the foundation. They would make them achieve a project. It may
      be the one by which this cheapness is only now.",
-  user: User.all.sample,
-  likes: [*(1..5)].sample
-  }
+   user: User.all.sample,
+   likes: [*(1..5)].sample
+   }
 )
 
 Post.create (
@@ -72,9 +72,9 @@ Post.create (
   It is something far more than that.
   It’s about taking back what is ours.
   Our freedom.",
-  user: User.all.sample,
-  likes: [*(1..5)].sample
-}
+   user: User.all.sample,
+   likes: [*(1..5)].sample
+   }
 )
 
 Post.create (
@@ -83,224 +83,227 @@ Post.create (
    the same result.",
    user: User.all.sample,
    likes: [*(1..5)].sample
- }
+   }
 )
 
 
- p "5 posts created"
+p "5 posts created"
 
 Cryptocurrency.create( {
-  name: "Bitcoin",
-  market_price:  6342.04,
-  abbreviation: "BTC"
+                         name: "Bitcoin",
+                         market_price:  6342.04,
+                         abbreviation: "BTC"
 })
 
 Cryptocurrency.create (
   {name: "Ethereum",
-  market_price: 278.28,
-  abbreviation: "ETC"}
+   market_price: 278.28,
+   abbreviation: "ETC"}
 )
 
 Cryptocurrency.create ( {
-  name: "XRP",
-  market_price: 0.327773,
-  abbreviation: "XRP"
+                          name: "XRP",
+                          market_price: 0.327773,
+                          abbreviation: "XRP"
 })
 
 Cryptocurrency.create ( {
-  name: "EOS",
-  market_price: 4.77,
-  abbreviation: "EOS"
+                          name: "EOS",
+                          market_price: 4.77,
+                          abbreviation: "EOS"
 })
 
 Cryptocurrency.create ( {
-  name: "Stellar",
-  market_price: 0.215311,
-  abbreviation: "XLM"
+                          name: "Stellar",
+                          market_price: 0.215311,
+                          abbreviation: "XLM"
 })
 
 Cryptocurrency.create ({
-  name: "Litecoin",
-  market_price: 54.59,
-  abbreviation: "LTC"
+                         name: "Litecoin",
+                         market_price: 54.59,
+                         abbreviation: "LTC"
 })
 
 Cryptocurrency.create ({
-  name: "Tether",
-  market_price: 1.00,
-  abbreviation: "USDT"
+                         name: "Tether",
+                         market_price: 1.00,
+                         abbreviation: "USDT"
 })
 
 Cryptocurrency.create ({
-  name: "Cardano",
-  market_price: 0.093063,
-  abbreviation: "ADA"
+                         name: "Cardano",
+                         market_price: 0.093063,
+                         abbreviation: "ADA"
 })
 
 Cryptocurrency.create ({
-  name: "Monero",
-  market_price: 93.38,
-  abbreviation: "XMR"
+                         name: "Monero",
+                         market_price: 93.38,
+                         abbreviation: "XMR"
 })
 
 Cryptocurrency.create ({
-  name: "IOTA",
-  market_price: 0.503882,
-  abbreviation: "IOTA"
+                         name: "IOTA",
+                         market_price: 0.503882,
+                         abbreviation: "IOTA"
 })
 
 p "10 cryptos created"
 
 Comment.create ({
-  content: "Spurt Plus (SPU+) owns and operates on its own Blockchain.
+                  content: "Spurt Plus (SPU+) owns and operates on its own Blockchain.
    Stay tuned for our Blockchain announcement, amongst other major deals
     signed off by Spu+. ",
-  user: User.all.sample,
-  post: Post.all.sample
+                  user: User.all.sample,
+                  post: Post.all.sample
 })
 
 
 Comment.create ({
-  content: "Total number of #bitcoins divided by the number of people
+                  content: "Total number of #bitcoins divided by the number of people
   on a planet is 21M/7B=0.003 #BTC. Total money supply is $90T, so for
    one person it is $90T/7B=$13k. ",
-  user: User.all.sample,
-  post: Post.all.sample })
+                  user: User.all.sample,
+post: Post.all.sample })
 
 Comment.create ({
-  content: "This is the problem with a world run by
+                  content: "This is the problem with a world run by
   FIAT Currencies.",
-  user: User.all.sample,
-  post: Post.all.sample
+                  user: User.all.sample,
+                  post: Post.all.sample
 })
 
 Comment.create ({
-  content: "Count me in",
-  user: User.all.sample,
-  post: Post.all.sample
+                  content: "Count me in",
+                  user: User.all.sample,
+                  post: Post.all.sample
 })
 
 Comment.create ({
-  content: "Awesome opportunity to be had",
-  user: User.all.sample,
-  post: Post.all.sample
+                  content: "Awesome opportunity to be had",
+                  user: User.all.sample,
+                  post: Post.all.sample
 })
 
 Comment.create ({
-  content: "A hugely encouraging rally from #Tezos.",
-  user: User.all.sample,
-  post: Post.all.sample
+                  content: "A hugely encouraging rally from #Tezos.",
+                  user: User.all.sample,
+                  post: Post.all.sample
 })
 
 Comment.create ({
-  content: "How should governments and Central Banks think about
+                  content: "How should governments and Central Banks think about
   monetary policy in the age of #cryptocurrencies and #cryptoassets?",
-  user: User.all.sample,
-  post: Post.all.sample
+                  user: User.all.sample,
+                  post: Post.all.sample
 })
 
 Comment.create ({
-  content: "RP and Ripple for the future! The basic thing of
+                  content: "RP and Ripple for the future! The basic thing of
   economy, money transfer should move into 2018.
    Atm it is stuck in the 70's. ",
-  user: User.all.sample,
-  post: Post.all.sample
+                  user: User.all.sample,
+                  post: Post.all.sample
 })
 
 
 Comment.create ({
-  content: "@ProfFaustus interesting thoughts about stability.
+                  content: "@ProfFaustus interesting thoughts about stability.
    Lucky them who already have large adoption! ;-P",
-  user: User.all.sample,
-  post: Post.all.sample
+                  user: User.all.sample,
+                  post: Post.all.sample
 })
 
 Comment.create ({
-  content: "A promising project in the crypto currency market.
+                  content: "A promising project in the crypto currency market.
    Don't miss opportunity for investments",
-  user: User.all.sample,
-  post: Post.all.sample
+                  user: User.all.sample,
+                  post: Post.all.sample
 })
 
 Comment.create ({
-  content: "Our smart contracts allow agreements to be made between users
+                  content: "Our smart contracts allow agreements to be made between users
    and ensures that the aqreements are carried out religiously.",
-  user: User.all.sample,
-  post: Post.all.sample
+                  user: User.all.sample,
+                  post: Post.all.sample
 })
 
 Comment.create ({
-  content: "I don’t understand how people can look at the things TRX has
+                  content: "I don’t understand how people can look at the things TRX has
   been doing and still think it’s going to fail. 99% sure it’s
   hey’re doing more than your shitcoin. ",
-  user: User.all.sample,
-  post: Post.all.sample
+                  user: User.all.sample,
+                  post: Post.all.sample
 })
 
 Comment.create ({
-  content: "Biggest problem with #trx is education.  Don’t think the crypto
+                  content: "Biggest problem with #trx is education.  Don’t think the crypto
    community understands what exactly BitTorrent can do for all crypto and
     the trx platform. The BitTorrent protocol touches so much and puts it ahead
      of most platforms. ",
-  user: User.all.sample,
-  post: Post.all.sample
+                  user: User.all.sample,
+                  post: Post.all.sample
 })
 
 Comment.create ({
-  content: "Bittorrent is everywhere..) ",
-  user: User.all.sample,
-  post: Post.all.sample
- })
-
-Comment.create ({
-  content: "BTC is awesome",
-  user:User.all.sample,
-  post: Post.all.sample
+                  content: "Bittorrent is everywhere..) ",
+                  user: User.all.sample,
+                  post: Post.all.sample
 })
 
 Comment.create ({
-  content: "Will not fail AT ALL!!! ,7 EOY ",
-  user: User.all.sample,
-  post: Post.all.sample
+                  content: "BTC is awesome",
+                  user:User.all.sample,
+                  post: Post.all.sample
 })
 
 Comment.create ({
-  content: "Tron has accomplished more in 1 year than 85% of
+                  content: "Will not fail AT ALL!!! ,7 EOY ",
+                  user: User.all.sample,
+                  post: Post.all.sample
+})
+
+Comment.create ({
+                  content: "Tron has accomplished more in 1 year than 85% of
    the coins on coinmarketcap's top 100 list no matter how long
     they've been around",
-  user: User.all.sample,
-  post: Post.all.sample
+                  user: User.all.sample,
+                  post: Post.all.sample
 })
 
 Comment.create ({
-  content: "Asian market is the most important considering
+                  content: "Asian market is the most important considering
    the quantity of the population... ",
-  user: User.all.sample,
-  post: Post.all.sample
+                  user: User.all.sample,
+                  post: Post.all.sample
 })
 
 Comment.create ({
-  content: "ETC is cool",
-  user: User.all.sample,
-  post: Post.all.sample
+                  content: "ETC is cool",
+                  user: User.all.sample,
+                  post: Post.all.sample
 })
 
 puts "20 comments created"
+
+10.times do
+  Portfolio.create (
+    {name: Faker::HarryPotter.character,
+     user: User.all.sample
+     }
+  )
+end
+
+puts "10 portfolios created"
 
 30.times do
   Ownedcurrency.create(
     {quantity: [*(1..500)].sample,
      cryptocurrency: Cryptocurrency.all.sample,
      portfolio: Portfolio.all.sample
-  }
+     }
   )
 end
 
-10.times do
-Portfolio.create (
-  {name: Faker::HarryPotter.character,
-  user: User.all.sample
-}
-)
-end
-
+puts "30 ownedcurrencies created"
