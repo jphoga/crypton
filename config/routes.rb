9330efa8e34f077ec53259382b08a3552ffab5fc
Jan Hoga <janpaulhoga@gmail.com>
2018-08-22 10:48:15 +0900

    brushed up portfolio routes and links

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,11 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/portfolio', to: 'portfolios#index', as: 'portfolio'
   resources :portfolios, only: [ :new, :create, :destroy ] do
+    resources :ownedcurrencies, only: [:create]
   end
   resources :posts do
     resources :comments
   end
   resources :comments, only: [:destroy]
-  resources :ownedcurrencies, only: [ :index, :show, :new, :create, :destroy ]
+  resources :ownedcurrencies, only: [ :index, :show, :new, :destroy ]
 end
