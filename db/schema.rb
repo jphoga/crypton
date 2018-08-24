9091907fe180bfd942d35dac89d2aef5ebbc2c74
David Li <David.Li920@gmail.com>
2018-08-24 10:50:18 +0900

    worked on saving favorite cards

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_23_050206) do
+ActiveRecord::Schema.define(version: 2018_08_23_081025) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,6 +44,7 @@ ActiveRecord::Schema.define(version: 2018_08_23_050206) do
     t.string "abbreviation"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "website_slug"
   end
 
   create_table "favorites", force: :cascade do |t|
