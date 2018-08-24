6592a70155945aeb120c21e79aa4181edd6a1308
Jan Hoga <janpaulhoga@gmail.com>
2018-08-24 12:04:05 +0900

    created solution for portfolio with market_price == nil

--- app/models/cryptocurrency.rb
+++ app/models/cryptocurrency.rb
@@ -2,6 +2,10 @@ class Cryptocurrency < ApplicationRecord
   acts_as_favoritable
 
   def total_owned_value(ownedcurrency)
-    market_price * ownedcurrency.quantity
+    if market_price == nil
+      return 0
+    else
+      return market_price * ownedcurrency.quantity
+    end
   end
 end
