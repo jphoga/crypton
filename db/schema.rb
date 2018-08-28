7b41242736a7fbbf50c4ba37fa78be973b9e6d8e
Hiroki Furuichi <aivvinna@gmail.com>
2018-08-28 19:30:12 +0900

    user can now upload pictures properly, other formatting for profile page done

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_27_083037) do
+ActiveRecord::Schema.define(version: 2018_08_28_090556) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -131,8 +131,10 @@ ActiveRecord::Schema.define(version: 2018_08_27_083037) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "photo"
+    t.string "username"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
   create_table "votes", id: :serial, force: :cascade do |t|
