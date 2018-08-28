a9fb6baaffd551107a42e44a5f0c43b81e22ba30
Jan Hoga <janpaulhoga@gmail.com>
2018-08-28 23:14:52 +0900

    added hide button

--- app/javascript/packs/news_toggle.js
+++ app/javascript/packs/news_toggle.js
@@ -3,6 +3,7 @@ const showhideNews = (url) => {
     const prices = document.querySelector(".prices-container");
     const news = document.querySelector(".all-news-container");
     const frameContainer = document.getElementById("news-main-box");
+    const buttons = document.querySelectorAll("#show-news");
 
 
     const news_frame = '<iframe style=" border: none;margin-top: 20px ; min-width: 880px; min-height: 1500px;" src="' + url + '"></iframe>'
@@ -11,9 +12,16 @@ const showhideNews = (url) => {
     if (prices.style.display === "none") {
         prices.style.display = "block";
         news.style.display = "none";
+        buttons.forEach(function(button) {
+          button.innerHTML = "Expand news";
+        })
     } else {
         prices.style.display = "none";
         news.style.display = "block";
+        buttons.innerHTML = "Hide news";
+        buttons.forEach(function(button) {
+          button.innerHTML = "Hide news";
+        })
     }
 }
 
