6614efcafc3dfca19879af915128120e98fef74a
Jan Hoga <janpaulhoga@gmail.com>
2018-08-29 23:12:36 +0900

    expand news closes posts and only one button to say hide

--- app/javascript/packs/news_toggle.js
+++ app/javascript/packs/news_toggle.js
@@ -4,28 +4,32 @@ const showhideNews = (url) => {
     const news = document.querySelector(".all-news-container");
     const frameContainer = document.getElementById("news-main-box");
     const buttons = document.querySelectorAll("#show-news");
+    var postsContainer = document.querySelector('.post-partial-container');
 
 
     const news_frame = '<iframe style=" border: none;margin-top: 20px ; min-width: 880px; min-height: 1500px;" src="' + url + '"></iframe>'
     frameContainer.innerHTML = news_frame;
 
-    if (prices.style.display === "none") {
-        prices.style.display = "block";
-        // console.log("YOPO");
-        news.style.display = "none";
-        buttons.forEach(function(button) {
-          button.innerHTML = "Expand news";
-        })
-    } else {
-        prices.style.display = "none";
-        news.style.display = "block";
-        // console.log("YYYOOOO");
-        // news.style.transform = "scale(1)";
-        buttons.innerHTML = "Hide news";
-        buttons.forEach(function(button) {
-          button.innerHTML = "Hide news";
-        })
-    }
+    if (postsContainer.style.display === "block") {
+      postsContainer.style.display = "none";
+      news.style.display = "block";
+    } else if (prices.style.display === "block") {
+      prices.style.display = "none";
+      news.style.display = "block";
+      // buttons.forEach(function(button) {
+      //   button.innerHTML = "Expand news";
+      // })
+      } else {
+      prices.style.display = "block";
+      news.style.display = "none";
+      // console.log("YYYOOOO");
+      // news.style.transform = "scale(1)";
+      // buttons.innerHTML = "Hide news";
+      // buttons.forEach(function(button) {
+      //   button.innerHTML = "Hide news";
+      // })
+  }
 }
+
 
 export default showhideNews;
