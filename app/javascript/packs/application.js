aa38344b1b013405976f3ffa77096021143fbe0e
Jan Hoga <janpaulhoga@gmail.com>
2018-08-30 00:20:46 +0900

    small changes but actually not much changed

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,12 +3,13 @@ import "currency.js";
 import '../components/select2';
 import showhideNews from "./news_toggle";
 
+
 document.querySelectorAll(".show-news").forEach((button) => {
   console.log("start JS");
   button.addEventListener("click", (event) => {
     console.log(event.currentTarget.dataset.url);
-    showhideNews(event.currentTarget.dataset.url);
-    if (event.currentTarget.innerHTML === "Hide news") {
+    showhideNews(event.currentTarget.innerHTML, event.currentTarget.dataset.url);
+    if (event.currentTarget.innerHTML === "Hide news" ) {
       event.currentTarget.innerHTML = "Expand news";
     } else {
         event.currentTarget.innerHTML = "Hide news";
