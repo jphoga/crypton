a91552747bb1cd4a8922917a1e11d16b5f3183a6
David Li <David.Li920@gmail.com>
2018-08-29 14:30:23 +0900

    changed top section

--- app/javascript/packs/coinmarketcap_api.js
+++ app/javascript/packs/coinmarketcap_api.js
@@ -1,3 +1,5 @@
+import "currency.js";
+
 fetch('https://api.coinmarketcap.com/v2/global/')
 .then(function(response){
   return response.json();
@@ -5,7 +7,7 @@ fetch('https://api.coinmarketcap.com/v2/global/')
 .then(function(data){
   const marketCap         = document.getElementById("marketcap");
   const marketCapValue    = data.data.quotes.USD.total_market_cap;
-  marketCap.innerText     = "$" + marketCapValue;
+  marketCap.innerText     = "$" + Number(marketCapValue.toFixed(0)).toLocaleString('en');
 
   // const lastUpdated       = document.getElementById("last-updated");
   // const lastUpdatedValue  = data.data.last_updated;
@@ -17,11 +19,11 @@ fetch('https://api.coinmarketcap.com/v2/global/')
 
   const cryptoCount       = document.getElementById("cryptocurrency-number");
   const cryptoCountValue  = data.data.active_cryptocurrencies;
-  cryptoCount.innerText   = cryptoCountValue;
+  cryptoCount.innerText   = Number(cryptoCountValue).toLocaleString('en');
 
   const dailyVolume       = document.getElementById("daily-volume");
   const dailyVolumeValue  = data.data.quotes.USD.total_volume_24h;
-  dailyVolume.innerText   = "$" + dailyVolumeValue;
+  dailyVolume.innerText   = "$" + Number(dailyVolumeValue.toFixed(0)).toLocaleString('en');
 });
 
 fetch('https://api.coinmarketcap.com/v2/ticker/1/')
@@ -31,5 +33,9 @@ fetch('https://api.coinmarketcap.com/v2/ticker/1/')
 .then(function(data){
   const bitcoin           = document.getElementById("bitcoin-price");
   const bitcoinValue      = data.data.quotes.USD.price
-  bitcoin.innerText       = "$" + bitcoinValue;
+  bitcoin.innerText       = "$" + Number(bitcoinValue.toFixed(0)).toLocaleString('en');
+
 });
+
+
+// Number(10000).toLocaleString('en');  // "10,000"
