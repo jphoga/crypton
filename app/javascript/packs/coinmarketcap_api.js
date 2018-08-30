f5bcf5e4ecb19187bf7e7455be3afe67bd383f94
Hiroki Furuichi <aivvinna@gmail.com>
2018-08-30 15:01:49 +0900

    added if statements in the coinmarketcap js

--- app/javascript/packs/coinmarketcap_api.js
+++ app/javascript/packs/coinmarketcap_api.js
@@ -7,7 +7,9 @@ fetch('https://api.coinmarketcap.com/v2/global/')
 .then(function(data){
   const marketCap         = document.getElementById("marketcap");
   const marketCapValue    = data.data.quotes.USD.total_market_cap;
-  marketCap.innerText     = "$" + Number(marketCapValue.toFixed(0)).toLocaleString('en');
+  if (marketCap) {
+    marketCap.innerText     = "$" + Number(marketCapValue.toFixed(0)).toLocaleString('en');
+  }
 
   // const lastUpdated       = document.getElementById("last-updated");
   // const lastUpdatedValue  = data.data.last_updated;
@@ -15,15 +17,21 @@ fetch('https://api.coinmarketcap.com/v2/global/')
 
   const btcDominance      = document.getElementById("bitcoin-dominance");
   const btcDominanceValue = data.data.bitcoin_percentage_of_market_cap;
-  btcDominance.innerText  = btcDominanceValue + "%";
+  if (btcDominance) {
+    btcDominance.innerText  = btcDominanceValue + "%";
+  }
 
   const cryptoCount       = document.getElementById("cryptocurrency-number");
   const cryptoCountValue  = data.data.active_cryptocurrencies;
-  cryptoCount.innerText   = Number(cryptoCountValue).toLocaleString('en');
+  if (cryptoCount) {
+    cryptoCount.innerText   = Number(cryptoCountValue).toLocaleString('en');
+  }
 
   const dailyVolume       = document.getElementById("daily-volume");
   const dailyVolumeValue  = data.data.quotes.USD.total_volume_24h;
-  dailyVolume.innerText   = "$" + Number(dailyVolumeValue.toFixed(0)).toLocaleString('en');
+  if (dailyVolume) {
+    dailyVolume.innerText   = "$" + Number(dailyVolumeValue.toFixed(0)).toLocaleString('en');
+  }
 });
 
 fetch('https://api.coinmarketcap.com/v2/ticker/1/')
@@ -33,7 +41,9 @@ fetch('https://api.coinmarketcap.com/v2/ticker/1/')
 .then(function(data){
   const bitcoin           = document.getElementById("bitcoin-price");
   const bitcoinValue      = data.data.quotes.USD.price
-  bitcoin.innerText       = "$" + Number(bitcoinValue.toFixed(0)).toLocaleString('en');
+  if (bitcoin) {
+    bitcoin.innerText       = "$" + Number(bitcoinValue.toFixed(0)).toLocaleString('en');
+  }
 
 });
 
