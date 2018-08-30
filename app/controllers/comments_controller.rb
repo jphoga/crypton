ff5031161bcb4650d7a36fac1d04511f96d651f7
Hiroki Furuichi <aivvinna@gmail.com>
2018-08-30 21:11:17 +0900

    delete works on comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -18,6 +18,13 @@ class CommentsController < ApplicationController
     end
   end
 
+  def destroy
+    @comment = Comment.find(params[:id])
+    authorize @comment
+    @comment.destroy
+    redirect_to user_path(@comment.user)
+  end
+
   private
 
   def comment_params
