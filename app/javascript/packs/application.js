995a944bae99ed4772075547097cad8bf1de81d0
Hiroki Furuichi <aivvinna@gmail.com>
2018-08-30 10:48:31 +0900

    works for news too

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,24 +9,24 @@ document.querySelectorAll(".show-news").forEach((button) => {
   button.addEventListener("click", (event) => {
     console.log(event.currentTarget.dataset.url);
     showhideNews(event.currentTarget.innerHTML, event.currentTarget.dataset.url);
-    if (event.currentTarget.innerHTML === "Hide news" ) {
-      event.currentTarget.innerHTML = "Expand news";
-    } else {
-        event.currentTarget.innerHTML = "Hide news";
-    };
+    // if (event.currentTarget.innerHTML === "Hide news" ) {
+    //   event.currentTarget.innerHTML = "Expand news";
+    // } else {
+    //     event.currentTarget.innerHTML = "Hide news";
+    // };
   })
 })
 
-var button = document.getElementById("back_to_dashboard");
-button.addEventListener("click", (event) => {
-  console.log(event);
-  var postsContainer = document.querySelector('.post-partial-container');
-  var prices = document.querySelector(".prices-container");
-  postsContainer.style.display = "none"
-  prices.style.display = "block";
-  back.style.display = "none";
+// var button = document.getElementById("back_to_dashboard");
+// button.addEventListener("click", (event) => {
+//   console.log(event);
+//   var postsContainer = document.querySelector('.post-partial-container');
+//   var prices = document.querySelector(".prices-container");
+//   postsContainer.style.display = "none"
+//   prices.style.display = "block";
+//   back.style.display = "none";
 
-})
+// })
 
 
 
--- app/javascript/packs/news_toggle.js
+++ app/javascript/packs/news_toggle.js
@@ -1,30 +1,30 @@
 const showhideNews = (button, url) => {
     console.log("start of news_toggle")
     const prices = document.querySelector(".prices-container");
-    const news = document.querySelector(".all-news-container");
-    const frameContainer = document.getElementById("news-main-box");
-    const buttons = document.querySelectorAll(".show-news")
-    var postsContainer = document.querySelector('.post-partial-container');
+    // const news = document.querySelector(".all-news-container");
+    // const frameContainer = document.getElementById("news-main-box");
+    // const buttons = document.querySelectorAll(".show-news")
+    // var postsContainer = document.querySelector('.post-partial-container');
 
+    var backButton = `<div class="prices-back-right"> <a data-remote="true" href='/prices'> <i class="fas fa-angle-double-left"></i></a></div>`
+    var news_frame = '<iframe style=" border: none;margin-top: 20px ; min-width: 880px; min-height: 1500px;" src="' + url + '"></iframe>'
+    prices.innerHTML = backButton + news_frame;
 
-    const news_frame = '<iframe style=" border: none;margin-top: 20px ; min-width: 880px; min-height: 1500px;" src="' + url + '"></iframe>'
-    frameContainer.innerHTML = news_frame;
+  //   if (postsContainer.style.display === "block") {
+  //     postsContainer.style.display = "none";
+  //     news.style.display = "block";
+  //     console.log("inside if");
 
-    if (postsContainer.style.display === "block") {
-      postsContainer.style.display = "none";
-      news.style.display = "block";
-      console.log("inside if");
+  //   } else if (prices.style.display === "block") {
+  //     prices.style.display = "none";
+  //     news.style.display = "block";
+  //     console.log("inside else if");
 
-    } else if (prices.style.display === "block") {
-      prices.style.display = "none";
-      news.style.display = "block";
-      console.log("inside else if");
-
-  } else {
-      news.style.display = "none";
-      prices.style.display = "block";
-      console.log("inside else");
-  }
+  // } else {
+  //     news.style.display = "none";
+  //     prices.style.display = "block";
+  //     console.log("inside else");
+  // }
 }
 
 
